27e523227536997576bc9b4de309fe88f78a9474
unknown <guideox@hotmail.com>
2024-02-08 10:24:54 +0700

    codebuilding

--- Assets/Scripts/UI/MainUI.cs
+++ Assets/Scripts/UI/MainUI.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MainUI : MonoBehaviour
 {
@@ -10,6 +11,12 @@ public class MainUI : MonoBehaviour
     {
         get { return selectionMarker; }
     }
+
+    [SerializeField] private TextMeshProUGUI unitCountText;
+    [SerializeField] private TextMeshProUGUI foodText;
+    [SerializeField] private TextMeshProUGUI woodText;
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI stoneText;
 
     public static MainUI instance;
         void Awake()
@@ -27,5 +34,14 @@ public class MainUI : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    public void UpdateAllResource(Faction faction)
+    {
+        unitCountText.text = faction.AliveUnits.Count.ToString();
+        foodText.text = faction.Food.ToString();
+        woodText.text = faction.Wood.ToString();
+        goldText.text = faction.Gold.ToString();
+        stoneText.text = faction.Stone.ToString();
     }
 }
