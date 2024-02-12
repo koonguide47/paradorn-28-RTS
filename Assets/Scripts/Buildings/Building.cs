64d31408c3a7a31730328cdb45e80c8112ca1b91
koonguide47 <79179572+koonguide47@users.noreply.github.com>
2024-02-12 21:43:44 +0700

    unit2

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -28,6 +28,30 @@ public class Building : Structure
     {
         if (Input.GetKeyDown(KeyCode.G))
             ToCreateUnit(0);
+        
+
+        if ((recruitList.Count > 0) && (recruitList[0] != null))
+        {
+            unitTimer += Time.deltaTime;
+            curUnitWaitTime = recruitList[0].UnitWaitTime;
+
+            if (unitTimer >= curUnitWaitTime)
+            {
+                curUnitProgress++;
+                unitTimer = 0f;
+
+                if (curUnitProgress >= 100)
+                {
+                    curUnitProgress = 0;
+                    curUnitWaitTime = 0f;
+                    CreateUnitCompleted();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+            ToCreateUnit(2);
+        
 
         if ((recruitList.Count > 0) && (recruitList[0] != null))
         {
